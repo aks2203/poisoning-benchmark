0ad8738f833e662d6ddf11cf1869e29072b093c1
Avi Schwarzschild <avi1@math.umd.edu>
2020-09-26 16:20:41 -0400

    new tiny imagenet stuff.

--- benchmark_tiny.sh
+++ benchmark_tiny.sh
@@ -11,11 +11,11 @@
 
 if (( $# == 2 )); then
   for filepath in $1*/; do
-   echo "python tinyimagenet_poison_test.py --poisons_path ${filepath} --$2 --output benchmark_100_trials_tiny" >> backdoor_test_from_scratch_batches.sh
+   echo "python tinyimagenet_poison_test.py --poisons_path ${filepath} --$2 --output benchmark_100_trials_tiny" >> benchmark_test_from_scratch_batches.sh
   done
 elif (( $# == 1 )); then
   for filepath in $1*/; do
-    echo "python tinyimagenet_poison_test.py --poisons_path ${filepath} --output benchmark_100_trials_tiny" >> backdoor_test_batches.sh
+    echo "python tinyimagenet_poison_test.py --poisons_path ${filepath} --output benchmark_100_trials_tiny" >> benchmark_test_batches.sh
   done
 else
   echo "Illegal number of arguments."
